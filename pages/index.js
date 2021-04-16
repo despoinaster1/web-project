d4a820cdf24b838678afb0434137bbc2d61d0ac4
despoinaster1 <ceid5656@upnet.gr>
2021-04-17 00:11:51 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,75 @@
-import Head from 'next/head'
+//import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+function Form() {
+  const registerUser = async event => {
+    event.preventDefault()
+
+    const res = await fetch(
+      'https://hooks.zapier.com/hooks/catch/123456/abcde',
+      {
+        body: JSON.stringify({
+          name: event.target.name.value
+        }),
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        method: 'POST'
+      }
+    )
+
+    const result = await res.json()
+    // result.user => 'Ada Lovelace'
+  }
+
+  return (
+    <form onSubmit={registerUser}>
+      <label htmlFor="name">Name</label>
+      <input id="name" name="name" type="text" autoComplete="name" required />
+      <button type="submit">Register</button>
+    </form>
+  )
+}
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
+    
+      
       <main className={styles.main}>
+
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="http://localhost:3000/">our page!</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <h1 className={styles.description}>
+          Insert your personal details{' '}
+          <code className={styles.code}>username,password,email!</code>
+        </h1>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
+          
             <h3>Documentation &rarr;</h3>
             <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+            
+            <h3>Learn &rarr; </h3> 
+            <p> Learn about Next.js in an interactive course with quizzes! </p>
+         
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
             <h3>Examples &rarr;</h3>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
+          
             <h3>Deploy &rarr;</h3>
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
-          </a>
-        </div>
-      </main>
+        </div>    
+     <script> function Form() </script>
+      
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
-}
+    
+      </main> 
+    ) 
+  
+  }
+     
+   
